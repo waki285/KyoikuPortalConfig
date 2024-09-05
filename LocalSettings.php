4be5d54916251dca75a64fcdf95f5d6f536dd716
すずねーう <suzuneu@suzuneu.com>
2024-09-05 23:29:47 +0900

    erorr

--- LocalSettings.php
+++ LocalSettings.php
@@ -16,6 +16,10 @@ if ( !defined( 'CACHE_MEMCACHED' ) ) {
 	die( 'Not an entry point.' );
 }
 
+if ( !defined( 'MW_ENTRY_POINT' ) ) {
+	die( 'Not an entry point.' );
+}
+
 // for debugging
 // error_reporting( -1 );
 // ini_set( 'display_errors', 1 );
@@ -1046,6 +1050,23 @@ require_once __DIR__ . '/ManageWikiSettings.php';
 
 //var_dump($wgConf->settings);
 require_once "$IP/config/Database.php";
+
+
+if ( $wi->missing ) {
+	require_once '/var/www/mediawiki/ErrorPages/MissingWiki.php';
+}
+
+if ( $cwDeleted ) {
+	if ( MW_ENTRY_POINT === 'cli' ) {
+		wfHandleDeletedWiki();
+	} else {
+		define( 'MW_FINAL_SETUP_CALLBACK', 'wfHandleDeletedWiki' );
+	}
+}
+
+function wfHandleDeletedWiki() {
+	require_once '/var/www/mediawiki/ErrorPages/DeletedWiki.php';
+}
 
 require_once "$IP/config/GlobalSettings.php";
 
